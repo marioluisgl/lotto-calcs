1ba438f7936212814b664be9e9afc0f4936b1889
Mario Luis <mario@truedebug.com>
2023-10-02 15:10:46 -0500

    fix: api url deploy ++

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     nameWeb: 'LOTTO CALCULATION SYSTEM',
     hostWeb: 'http://localhost:4200',  // must be the same in configs api file
-    hostApi: 'http://localhost:3000',
+    hostApi: 'https://lotto-api-services.onrender.com',
 };
